d2348a944e4cf605fc8e611aed8a9ede6abb2b2f
Peter Jurczyński <pjurczynski@gmail.com>
2013-10-22 15:26:34 +0200

    add semicolons due to concatenating files

--- public/assets/javascripts/app.js
+++ public/assets/javascripts/app.js
@@ -1,6 +1,6 @@
 (function(){
-  var remainingDaysAnchor = document.getElementById('remainingDays')
-  var nextMeet = new Date( remainingDaysAnchor.getAttribute('data-date') )
-  var today = new Date()
-  remainingDaysAnchor.innerHTML = time_remaining_in_days(today, nextMeet)
-})()
+  var remainingDaysAnchor = document.getElementById('remainingDays');
+  var nextMeet = new Date( remainingDaysAnchor.getAttribute('data-date') );
+  var today = new Date();
+  remainingDaysAnchor.innerHTML = time_remaining_in_days(today, nextMeet);
+})();
--- public/assets/javascripts/time_remaining.js
+++ public/assets/javascripts/time_remaining.js
@@ -1,23 +1,23 @@
-ONE_DAY = 1000*60*60*24
+ONE_DAY = 1000*60*60*24;
 var time_difference = function(startDate, endDate) {
-  return endDate - startDate
-}
+  return endDate - startDate;
+};
 
 var time_remaining_in_days = function(startDate, endDate) {
-  var difference = time_difference(startDate, endDate)
-  days = Math.ceil( Math.abs(difference/ONE_DAY) )
-  var after = ''
+  var difference = time_difference(startDate, endDate);
+  days = Math.ceil( Math.abs(difference/ONE_DAY) );
+  var after = '';
   if(difference < 0) {
-    after = ' ago'
+    after = ' ago';
   }
 
-  return days + ' ' + pluralize('day', days) + after
-}
+  return days + ' ' + pluralize('day', days) + after;
+};
 
 var pluralize = function(noun, count) {
-  var suffix = ''
+  var suffix = '';
   if( Math.abs(count) > 1 ) {
-    suffix = 's'
+    suffix = 's';
   }
-  return noun + suffix
-}
+  return noun + suffix;
+};
