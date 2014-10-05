c0cfba42e37d9a046616d63bf3534df110175e06
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-10-05 10:26:56 +0530

    Update date for next meetup after a long time

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  next_meetup_date = Date.parse('2014/4/5')
+  next_meetup_date = Date.parse('2014/11/1')
   @days = (next_meetup_date.mjd - Date.today.mjd).to_i
   erb :index
 end
