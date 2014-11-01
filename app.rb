1aa5871f31e712a57581a9306297b49086d0472f
Prathamesh Sonpatki <csonpatki@gmail.com>
2014-11-01 23:30:24 +0530

    Added december meetup date

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  next_meetup_date = Date.parse('2014/11/1')
+  next_meetup_date = Date.parse('2014/12/6')
   @days = (next_meetup_date.mjd - Date.today.mjd).to_i
   erb :index
 end
